f933065010974e900aa464aaafe26a5989a9edc9
Salah Elraies <s4l47.elraies@gmail.com>
2021-02-04 00:19:29 +0200

    Auther basics

---
+++ models/author.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const autherSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('Author', autherSchema);
